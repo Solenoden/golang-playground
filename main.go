5d8e0b853a8f719c150133b90e5b43240eb7562d
Gavin de Bruyn <gavindb@runninghill.co.za>
2021-02-01 10:25:44 +0200

    Learnt how to retrieve paramters from a request and how to loop over a slice.

--- main.go
+++ main.go
@@ -4,32 +4,48 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
 type Book struct {
-	Id   int    `json:"id"`
+	Id   string `json:"id"`
 	Name string `json:"name"`
 }
 
 var books []Book
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/api/v1/books", getBooks).Methods("GET")
-
 	mockBookData()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/api/v1/book", getBooks).Methods("GET")
+	r.HandleFunc("/api/v1/book/{id}", getBookById).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
 
 func mockBookData() {
-	books = append(books, Book{Id: 1, Name: "Deep work"})
-	books = append(books, Book{Id: 2, Name: "The pragmatic programmer"})
-	books = append(books, Book{Id: 3, Name: "Clean code"})
+	books = append(books, Book{Id: "1", Name: "Deep work"})
+	books = append(books, Book{Id: "2", Name: "The pragmatic programmer"})
+	books = append(books, Book{Id: "3", Name: "Clean code"})
 }
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(books)
+}
+
+func getBookById(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for _, item := range books {
+		if item.Id == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+
+	json.NewEncoder(w).Encode(&Book{})
 }
